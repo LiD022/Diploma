85a73f7d35646a5d4e949689d1b8bbe4f69d12ed
Nikita <nikita.istomin.02@mail.ru>
2023-06-27 01:43:51 +0300

    last commit

--- notebooks/final_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-source ~/.bashrc
-conda activate dev
-python bat_sample.py
--- notebooks/my_script.sh
+++
@@ -1,5 +0,0 @@
-conda activate dev
-python bat_sample.py --num_sample 3 --tran_model celebahq_bat_pretrain --up_model celebahq_up_pretrain --input_dir ../inference/sample --mask_dir ../inference/sample_mask --save_dir ../inference/sample_res
-
-
-yolo task=detect mode=predict model='/home/nikita/Desktop/diploma/Diploma/models/yolov8n-face.pt' conf=0.15 imgsz=1280 line_width=1 max_det=100 source='/home/nikita/Desktop/diploma/inference/sample/sample.png'
